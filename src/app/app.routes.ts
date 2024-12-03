e806646fe9501159f7d79df5592cfa3e134c1330
Thomas Clark <thomas.clark@bigtincan.com>
2024-12-04 10:36:19 +1100

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TodoListPageComponent,
+    title: 'Todo List',
+  },
+];
